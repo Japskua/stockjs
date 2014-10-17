a4124117b7bd607082d95df93b5c259b8ae2a6a1
Janne Parkkila <japskua@gmail.com>
2014-10-17 20:37:20 +0300

    Added different routes for historical and snapshot data

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var stocks = require('./routes/stocks');
+var snapshot = require('./routes/snapshot');
+var historical = require('./routes/historical');
 
 var app = express();
 
@@ -26,6 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/stocks', stocks);
+app.use('/stocks/historical', historical);
+app.use('/stocks/snapshot', snapshot);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/historical.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Janne on 17.10.2014.
+ */
+var express = require('express');
+var router = express.Router();
+var StockManager = require('./../js/stock-manager');
+
+/* GET stocks listing. */
+router.get('/', function(req, res) {
+
+    // Get the results from a stockmanager
+    var stockManager = new StockManager();
+    stockManager.GetHistorical(function (error, result) {
+        if (error) {
+            res.send('respond with error: ', error);
+        }
+
+        res.send("Result is:", result);
+
+    });
+
+
+});
+
+module.exports = router;
